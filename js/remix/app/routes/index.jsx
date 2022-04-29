9f12bf3fea8a5f35a79569eccc0ccb99b01bc506
疒奀 <46226846+mlegls@users.noreply.github.com>
2022-04-29 16:00:07 -0500

    fix stuff I forgot to save

--- js/remix/app/routes/index.jsx
+++ js/remix/app/routes/index.jsx
@@ -1,4 +1,4 @@
 function Index() {
-    return <button onClick={(function () {    alert('hi!');    __PS_MV_REG = [];    return null;}).bind(this)}>{1 + 1}</button>;
+    return <button onClick={(function () {    alert('Hello!!!');    __PS_MV_REG = [];    return null();}).bind(this)}>{'Click ' + 'me!'}</button>;
 };
 module.exports = Index;
