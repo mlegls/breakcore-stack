e87d4fc44e7125b5a80a7d6b4a372664d0f80c59
疒奀 <46226846+mlegls@users.noreply.github.com>
2022-04-30 14:14:47 -0500

    minor cleanup

--- js/remix/app/routes/index.jsx
+++ js/remix/app/routes/index.jsx
@@ -1,9 +1,9 @@
-import { Button, Alert } from 'antd';
+import { Button, Alert, Space } from 'antd';
 function Index() {
-    return <div><Button onClick={(function () {
-    alert('Hello!!!');
+    return <div className="bg-black w-full p-2"><Space direction="vertical"><Alert message="Hello from the Remix Breakcore Stack!" type="success"></Alert><Button onClick={(function () {
+    alert('It works.');
     __PS_MV_REG = [];
     return null();
-}).bind(this)} type="primary">Click Me!</Button><Alert message="Hello from the Remix Breakcore Stack!" type="success"></Alert></div>;
+}).bind(this)} type="primary">Click Me :)</Button></Space></div>;
 };
 module.exports = Index;
