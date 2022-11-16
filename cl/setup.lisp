cf9fa538fe690b85a16fa6a30d0b32fd7fa9c38c
疒奀 <46226846+mlegls@users.noreply.github.com>
2022-11-16 10:23:16 -0800

    add changes from common-solution-market

--- cl/setup.lisp
+++ cl/setup.lisp
@@ -5,325 +5,9 @@
 (ql:quickload :rutils)
 (ql:quickload :arrows)
 
-(defpackage workspace
-  (:use #:cl)
-  (:export #:write-file
-           #:special-formatted))
-
-(in-package #:cl)
-(pushnew :parenscript *features*)
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (unless (named-readtables:find-readtable :parenscript)
-    (named-readtables:defreadtable :parenscript
-      (:merge :standard)
-      (:case #.(if (eql :upcase (readtable-case *readtable*))
-                   :invert
-                   (readtable-case *readtable*))))))
-
-(named-readtables:in-readtable :parenscript)
-
-(defpackage #:parenscript
-  (:use #:cl #:anaphora)
-  (:nicknames #:ps)
-  (:export
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Compiler interface
-
-   ;; compiler
-   #:*js-target-version*
-   #:ps
-   #:*parenscript-stream*
-   #:ps-to-stream
-   #:ps-doc
-   #:ps-doc*
-   #:ps*
-   #:ps-inline
-   #:ps-inline*
-   #:*ps-read-function*
-   #:ps-compile-file
-   #:ps-compile-stream
-   ;; for parenscript macro definition within lisp
-   #:defpsmacro
-   #:defmacro+ps
-   #:import-macros-from-lisp
-   #:*defined-operators*
-   #:*version*
-
-   ;; gensym
-   #:ps-gensym
-   #:with-ps-gensyms
-   #:ps-once-only
-   #:maybe-once-only
-   #:*ps-gensym-counter*
-
-   ;; naming and namespaces
-   #:in-package
-   #:use-package
-   #:ps-package-prefix
-   #:obfuscate-package
-   #:unobfuscate-package
-
-   ;; printer
-   #:symbol-to-js-string
-   #:*js-string-delimiter*
-   #:*js-inline-string-delimiter*
-   #:*ps-print-pretty*
-   #:*indent-num-spaces*
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Language
-
-   ;; literals
-   #:t
-   #:nil
-
-   ;; array literals
-   #:array
-   #:list
-   #:aref
-   #:elt
-   #:make-array
-   #:[]
-
-   ;; operators
-   ;; logical boolean
-   #:not
-   #:and
-   #:or
-
-   ;; bitwise boolean
-   #:logand
-   #:logior
-   #:logxor
-   #:lognot
-   #:ash
-
-   #:*
-   #:/
-   #:rem
-   #:mod
-   #:+
-   #:-
-   #:<
-   #:>
-   #:<=
-   #:>=
-   #:incf
-   #:decf
-   #:equal
-   #:eql
-   #:eq
-   #:=
-
-   ;; compile-time stuff
-   #:eval-when
-
-   ;; body forms
-   #:progn
-
-   ;; if
-   #:if
-   #:when
-   #:unless
-
-   ;; control flow
-   #:return
-   #:return-from
-   #:throw
-
-   ;; assignment and binding
-   #:setf
-   #:defsetf
-   #:psetf
-   #:setq
-   #:psetq
-   #:let*
-   #:let
-
-   ;; variables
-   #:defvar
-
-   ;; iteration
-   #:do
-   #:do*
-   #:dotimes
-   #:dolist
-   #:loop
-
-   ;; case
-   #:switch
-   #:case
-   #:default
-
-   ;; function definition
-   #:defun
-   #:async-defun
-   #:lambda
-   #:async-lambda
-   #:flet
-   #:labels
-
-   ;; lambda lists
-   #:&key
-   #:&rest
-   #:&body
-   #:&optional
-   #:&aux
-   #:&environment
-   #:&key-object
-
-   ;; macros
-   #:macrolet
-   #:symbol-macrolet
-   #:define-symbol-macro
-   #:define-ps-symbol-macro
-   #:defmacro
-
-   ;; utils
-   #:max
-   #:min
-   #:floor
-   #:ceiling
-   #:round
-   #:sin
-   #:cos
-   #:tan
-   #:asin
-   #:acos
-   #:atan
-   #:pi
-   #:sinh
-   #:cosh
-   #:tanh
-   #:asinh
-   #:acosh
-   #:atanh
-   #:1+
-   #:1-
-   #:abs
-   #:evenp
-   #:oddp
-   #:exp
-   #:expt
-   #:log
-   #:sqrt
-   #:random
-   #:ignore-errors
-   #:concatenate
-   #:length
-   #:stringp
-   #:numberp
-   #:functionp
-   #:append
-   #:apply
-   #:destructuring-bind
-
-   ;; js runtime utils
-   #:*ps-lisp-library*
-   #:mapcar
-   #:map-into
-   #:map
-   #:member
-   #:append
-   #:set-difference
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Non-Common Lisp functionality
-
-   ;; DOM accessing utils
-   #:inner-html
-   #:uri-encode
-   #:attribute
-   #:offset
-   #:scroll
-   #:inner
-   #:client
-
-   ;; utils
-   #:@
-   #:chain
-   #:defined
-   #:undefined
-   #:booleanp
-   #:objectp
-   #:stringify
-
-   ;; html generator for javascript
-   #:*ps-html-empty-tag-aware-p*
-   #:*ps-html-mode*
-   #:ps-html
-   #:who-ps-html
-
-   ;; lisp eval
-   #:lisp
-
-   ;; js object stuff
-   #:delete
-   #:typeof
-   #:instanceof
-   #:new
-   #:create
-
-   ;; slot access
-   #:with-slots
-   #:getprop
-   #:in
-
-   ;; literals
-   #:regex
-   #:this
-   #:undefined
-   #:{}
-   #:false
-
-   ;; iteration
-   #:for
-   #:for-in
-   #:while
-
-   ;; global var
-   #:var
-
-   ;; control flow
-   #:try
-   #:break
-   #:continue
-   #:await
-
-   ;; jsx
-   #:jsx
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Deprecated functionality
-
-   #:define-script-symbol-macro
-   #:gen-js-name
-   #:with-unique-js-names
-   #:defjsmacro
-   #:js-compile
-   #:js-inline
-   #:js-inline*
-   #:js
-   #:js*
-   #:symbol-to-js
-   #:slot-value
-   #:compile-script
-   #:defmacro/ps
-   #:%
-   #:==
-   #:===
-   #:!=
-   #:!==
-   #:labeled-for
-   #:do-set-timeout
-   #:concat-string
-   #:with
-   #:label
-   #:f
-   #:bind
-   #:bind*))
-
+(defpackage #:workspace
+  (:use #:cl #:parenscript)
+  (:export #:write-file #:make-js))
 
 (in-package :parenscript)
 ; compile to expression
@@ -332,6 +16,37 @@
     (parenscript-print (compile-expression `(progn ,@body)) t)
     (unless *parenscript-stream*
       (get-output-stream-string *psw-stream*))))
+
+; TODO fix __PS_MV_REG
+(defpsmacro multiple-value-bind (vars form &body body)
+  (let* ((form       (ps-macroexpand form))
+         (progn-form (when (and (consp form)
+                                (member (car form)
+                                        '(with label let flet labels
+                                           macrolet symbol-macrolet progn)))
+                       (pop form))))
+    (if progn-form
+        `(,progn-form
+           ,@(butlast form)
+           (multiple-value-bind ,vars
+               ,@(last form)
+             ,@body))
+        ;; assume function call
+        `(progn
+           (var __PS_MV_REG '())
+           (let ((,(car vars) ,form))
+             (destructuring-bind (&optional ,@(cdr vars))
+                 __PS_MV_REG
+               ,@body))))))
+
+(defpsmacro multiple-value-list (form)
+  (with-ps-gensyms (first-value values-list)
+    `(let* ((,first-value (progn
+                            (var __PS_MV_REG '())
+                            ,form))
+            (,values-list (funcall (getprop __PS_MV_REG 'slice))))
+       (funcall (getprop ,values-list 'unshift) ,first-value)
+       ,values-list)))
 
 ; jsx instead of html
 (defun formatted-jsx (body)
@@ -353,24 +68,24 @@
                                    (if (keywordp (car el-body))
                                        (if (consp (cadr el-body))
                                            (progn
-                                            (push (format nil " ~A=" (symbol-to-js-string (car el-body))) r)
-                                            (push (formatted-jsx (cadr el-body)) r)
-                                            (process-attributes (cddr el-body)))
+                                             (push (format nil " ~A=" (symbol-to-js-string (car el-body))) r)
+                                             (push (formatted-jsx (cadr el-body)) r)
+                                             (process-attributes (cddr el-body)))
                                            (progn
-                                            (push (format nil " ~A=\""
-                                                          (symbol-to-js-string (car el-body))) r)
-                                            (push (cadr el-body) r)
-                                            (push "\"" r)
-                                            (process-attributes (cddr el-body))))
-                                      el-body))))
-                       (let ((content (process-attributes (cdr form))))
-                         (if (or content (not (empty-tag-p (car form))))
-                             (progn (push ">" r)
-                                    (when content (map nil #'process-form content))
-                                    (push (format nil "</~A>" (symbol-to-js-string (car form))) r))
-                             (progn (when (eql *ps-html-mode* :xml)
-                                      (push "/" r))
-                                    (push ">" r))))))
+                                             (push (format nil " ~A=\""
+                                                           (symbol-to-js-string (car el-body))) r)
+                                             (push (cadr el-body) r)
+                                             (push "\"" r)
+                                             (process-attributes (cddr el-body))))
+                                       el-body))))
+                        (let ((content (process-attributes (cdr form))))
+                          (if (or content (not (empty-tag-p (car form))))
+                              (progn (push ">" r)
+                                     (when content (map nil #'process-form content))
+                                     (push (format nil "</~A>" (symbol-to-js-string (car form))) r))
+                              (progn (when (eql *ps-html-mode* :xml)
+                                       (push "/" r))
+                                     (push ">" r))))))
                      (t (push form r)))))
       (map nil #'process-form forms)
       (concat-constant-strings (reverse r)))))
@@ -383,34 +98,153 @@
   (apply #'psw (with-standard-io-syntax (process-html-forms-cl-who html-forms))))
 
 ;; literal
-(define-expression-operator lisp-raw (lisp-form)
+(define-expression-operator raw (lisp-form)
   `(ps-js:escape
-    ,lisp-form))
+     ,lisp-form))
 
 (defun lisp-raw (x) x)
 
+;; const and let
+(define-statement-operator defconstant (form val)
+  `(ps-js::defconstant ,(compile-expression form) ,(compile-expression val)))
+
+(defprinter ps-js::defconstant (form val)
+  (psw "const ") (ps-print form)
+  (psw " = ") (ps-print val))
+
+(define-statement-operator jlet (form &optional val)
+  `(ps-js::jlet ,(compile-expression form) ,(compile-expression val)))
+
+(defprinter ps-js::jlet (form &optional (val))
+  (psw "let ") (ps-print form)
+  (when val (psw " = ") (ps-print val)))
+
+;; destructured object
+(define-expression-operator dobj (&rest symbols)
+  `(ps-js::dobj ,(mapcar #'compile-expression symbols)))
+
+(defprinter ps-js::dobj (symbols)
+  "{ "(print-comma-delimited-list symbols)" }")
+
+;; flat destructured symbols
+(define-expression-operator flat (&rest symbols)
+  `(ps-js::flat ,symbols))
+
+(defprinter ps-js::flat (symbols)
+  (print-comma-delimited-list symbols))
+
+;; arrow functions
+(defun print-=> (args body)
+    (psw "(")
+    (loop for (arg . remaining) on args do
+        (psw (symbol-to-js-string arg)) (when remaining (psw ", ")))
+    (psw ") => ")
+    (ps-print body))
+
+(ps::define-expression-operator => (lambda-list &rest body)
+  (multiple-value-bind (effective-args effective-body)
+      (ps::parse-extended-function lambda-list body)
+    `(ps-js::=> ,effective-args
+       ,(let ((ps::*function-block-names* ()))
+          (ps::compile-function-body effective-args effective-body)))))
+
+(ps::defprinter ps-js::=> (args body-block)
+  (ps::print-=> args body-block))
+
+(ps::define-expression-operator async-=> (lambda-list &rest body)
+  (multiple-value-bind (effective-args effective-body)
+      (ps::parse-extended-function lambda-list body)
+    `(ps-js::async-=> ,effective-args
+       ,(let ((ps::*function-block-names* ()))
+          (ps::compile-function-body effective-args effective-body)))))
+
+(ps::defprinter ps-js::async-=> (args body-block)
+  (ps::psw "async ")
+  (ps::print-=> args body-block))
+         
 ;; async
 (load "/Volumes/EP_1TB/Development/remix-lisp-test/cl/async.lisp")
 
-(ps
-  (defun test ()
-    (psx (:div
-          (:h1 "hello")))))
 ;; arrows
 (import '(rutils:->
-          rutils:->>))
+           rutils:->>))
 (ps:import-macros-from-lisp
   '->
   '->>)
 
+;; import, export 
+(define-expression-operator as (arg1 arg2)
+  `(ps-js::as ,(compile-expression arg1) ,(compile-expression arg2)))
+
+(defprinter ps-js::as (arg1 arg2)
+  (ps-print arg1)
+  (psw " as ")
+  (ps-print arg2))
+
+(define-statement-operator esm-import (name &optional source)
+  `(ps-js::esm-import ,(compile-expression name) ,(compile-expression source)))
+
+(defprinter ps-js::esm-import (name &optional source)
+  (psw "import ")
+  (ps-print name)
+  (when source (psw " from ") (ps-print source)))
+
+(define-statement-operator esm-export (name &optional default?)
+  `(ps-js::esm-export ,(compile-expression name) ,(if (eq default? :default)
+                                                      t nil)))
+
+(defprinter ps-js::esm-export (name &optional default?)
+  (psw "export ")
+  (when default? (psw "default "))
+  (ps-print name))
+
+;; macros
+(defpsmacro ps-compat ()
+  `(var *__PS_MV_REG*))
+
+(defpsmacro import-many (&rest modules)
+  (cons 'progn
+   (mapcar (lambda (module) (if (consp module)
+                                (cons 'esm-import module)
+                                (list 'esm-import module)))
+           modules)))
+
+;; remix macros
+(defpsmacro export-remix (default &optional options)
+  (cons 'progn
+   (list (when default `(esm-export ,default :default))
+         (case options
+           (:l '(esm-export (dobj loader)))
+           (:a '(esm-export (dobj action)))
+           (:la '(esm-export (dobj loader action)))))))
+     
+(defpsmacro defaction (&rest body)
+  `(async-defun action (req)
+                (ps-compat)
+                (defconstant request (@ req request))
+                (defconstant body (await (chain request (form-data))))
+                ,@body))
+
+(defpsmacro defloader (&rest body)
+  `(async-defun loader (req)
+                (ps-compat)
+                (defconstant request (@ req request))
+                ,@body))
+
+;; utils
 (in-package :workspace)
 ;; write to file
 (defun write-file (name content)
   (with-open-file (stream name
-                        :direction :output
-                        :if-exists :supersede
-                        :if-does-not-exist :create)
+                          :direction :output
+                          :if-exists :supersede
+                          :if-does-not-exist :create)
     (write-string content stream)))
+
+(defmacro make-js (path &rest body)
+  `(workspace::write-file ,(format nil "~{~A~}" 
+                                   (list "/Volumes/EP_1TB/Development/common-ai/solution-market/app-cl/js/remix/app/" path))
+                          (ps::ps ,@body)))
 
 ;; editing namespace
 (ql:quickload :paren6)
@@ -420,14 +254,14 @@
         #:alexandria)
   (:shadowing-import-from #:parenscript #:switch)
   (:export
-   #:export
-   #:export-default
-   #:import
-   #:list6
-   #:create6
-   #:=>
-   #:defclass6
-   #:defconstant6
-   #:super
-   #:import-into
-   #:for-of))
+    #:export
+    #:export-default
+    #:import
+    #:list6
+    #:create6
+    #:=>
+    #:defclass6
+    #:defconstant6
+    #:super
+    #:import-into
+    #:for-of))
